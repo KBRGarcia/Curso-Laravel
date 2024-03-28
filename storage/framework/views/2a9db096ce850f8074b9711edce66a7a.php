7a838d6c399d357c9b10bcce23f621a967e723f8
KBRGarcia <=>
2024-03-27 22:49:00 -0400

    sdfasdf

---
+++ storage/framework/views/2a9db096ce850f8074b9711edce66a7a.php
@@ -0,0 +1,8 @@
+
+
+<?php $__env->startSection('title', 'Home'); ?>
+    
+<?php $__env->startSection('content'); ?>
+    <h1>Bienvenido a la pagina de principal</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.plantilla', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Codigos-KBR\Curso-Laravel\resources\views/home.blade.php ENDPATH**/ ?>
